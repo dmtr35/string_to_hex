1ba1b63680e80c85b614db800e2875700e3bfed0
dmtr35 <nuke-id@yandex.ru>
2024-05-26 15:31:21 +0300

    change string to hex of bytes (separated by 8 bytes)

---
+++ readme.txt
@@ -0,0 +1,6 @@
+gcc str_to_hex.c -o str_to_hex
+
+one argument: <string>
+
+exemple:
+str_to_hex hello_world
---
+++ str_to_hex.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main (int argc, char *argv[])
+{
+    char string[256];
+    strcpy(string, argv[1]);
+    size_t leng = strlen(argv[1]);
+    int parts = (leng + 7) / 8;
+    int remainder = leng - ((parts - 1) * 8);
+
+
+    int i = 0;
+    for (int j = 0; j < parts; ++j){
+        printf("0x");
+        char buf[8];
+        for (int k = 0; k < 8 && i < leng; ++i, ++k) {
+            buf[k] = string[i];
+        }
+
+        int p = 8;
+        if (j+1 == parts) {
+            p = remainder;
+        }
+
+        for (; p > 0; --p) {
+            printf("%02x", (unsigned char)buf[p-1]);
+        }
+            printf("\n");
+    }
+
+    return 0;
+}
